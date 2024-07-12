1609332a4f01779a9c628db16c0bb55ec9a1fc5a
Thomas Duckworth <tduck973564@gmail.com>
2024-07-12 23:18:53 +1000

    Add i18n

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,56 @@
-#!/bin/bash
-find -name \*.cpp -o -name \*.h -o -name \*.qml | sort | xargs xgettext --c++ --kde --from-code=UTF-8 \
--ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 \
--kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
--kkli18n:1 -kkli18nc:1c,2 -kkli18np:1,2 -kkli18ncp:1c,2,3 \
--kI18N_NOOP:1 -kI18NC_NOOP:1c,2 \
---copyright-holder="Thomas Duckworth" \
---msgid-bugs-address="tduck973564@gmail.com" \
---package-name="org.filotimo.atychia" \
---package-version="0.1" -o po/atychia.pot
+#!/bin/sh
+# https://techbase.kde.org/Development/Tutorials/Localization/i18n_Build_Systems/Outside_KDE_repositories
+
+BASEDIR=`pwd`	# root of translatable sources
+PROJECT="atychia"	# project name
+BUGADDR="https://github.com/filotimo-linux/atychia"	# MSGID-Bugs
+WDIR="`pwd`/po"		# working dir
+
+
+echo "Preparing rc files"
+cd ${BASEDIR}
+# we use simple sorting to make sure the lines do not jump around too much from system to system
+find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WDIR}/rcfiles.list
+xargs --arg-file=${WDIR}/rcfiles.list extractrc > ${WDIR}/rc.cpp
+# additional string for KAboutData
+echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WDIR}/rc.cpp
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WDIR}/rc.cpp
+cd ${WDIR}
+echo "Done preparing rc files"
+
+
+echo "Extracting messages"
+cd ${BASEDIR}
+# see above on sorting
+find . -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' | sort > ${WDIR}/infiles.list
+echo "rc.cpp" >> ${WDIR}/infiles.list
+cd ${WDIR}
+xgettext --c++ --kde --from-code=UTF-8 \
+    -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 \
+    -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+    -kkli18n:1 -kkli18nc:1c,2 -kkli18np:1,2 -kkli18ncp:1c,2,3 \
+    -kI18N_NOOP:1 -kI18NC_NOOP:1c,2 \
+    -kxi18n:1 -kxi18nc:1c,2 -kxi18np:1,2 -kxi18ncp:1c,2,3 \
+    -kkxi18n:1 -kkxi18nc:1c,2 -kkxi18np:1,2 -kkxi18ncp:1c,2,3 \
+    -kklxi18n:1 -kklxi18nc:1c,2 -kklxi18np:1,2 -kklxi18ncp:1c,2,3 \
+    --msgid-bugs-address="${BUGADDR}" \
+    --files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+echo "Done extracting messages"
+
+
+echo "Merging translations"
+catalogs=`find . -name '*.po'`
+for cat in $catalogs; do
+  echo $cat
+  msgmerge -o $cat.new $cat ${PROJECT}.pot
+  mv $cat.new $cat
+done
+echo "Done merging translations"
+
+
+echo "Cleaning up"
+cd ${WDIR}
+rm rcfiles.list
+rm infiles.list
+rm rc.cpp
+echo "Done"
